56dfb01458190530a38985aef8038594ed8fa28c
David <davidsaleh03@gmail.com>
2026-01-03 01:35:40 -0800

    next

--- Home/Home.jsx
+++ Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
 
 export const Home = () => {
   return (
     <div className='home'>
-
+      <Navbar />
     </div>
   )
 }
