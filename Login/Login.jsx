30efea473c85fb00c258e03bd2b0cc01bd9730ea
David <davidsaleh03@gmail.com>
2026-01-04 00:18:46 -0800

    sign in out

--- Login/Login.jsx
+++ Login/Login.jsx
@@ -1,38 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Logo from "../../assets/logo.png";
+import SignIn from "../../components/SignInUp/SignIn";
+import SignUp from "../../components/SignInUp/SignUp";
+import SignUpBtn from "../../components/SignInUp/SignUpBtn";
+import SignInBtn from "../../components/SignInUp/SignInBtn";
+
 
 const Login = () => {
+  const [log, setLog] = useState(false);
+
+  function logIn() {
+    setLog(prevLog => !prevLog);
+  }
+
   return (
     <div className="login">
       <div className="login__area">
         <div className="login__area--top">
         <div className="login__top">
           <img src={Logo} alt="" className="login__img" />
+          <div className="top__left">
           <select name="LanguageSelector" className="top__selector">
             <option>English</option>
           </select>
+          { log ? <SignUpBtn onClick={logIn} /> : <SignInBtn onClick={logIn}/>}
+          </div>
         </div>
         </div>
-        <div className="login__form">
-            <h1>Sign In</h1>
-            <form>
-                <input type="text" placeholder="Your Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button>Sign In</button>
-                <div className="form__help">
-                    <div className="remember">
-                        <input type="checkbox" />
-                        <label htmlFor="">Remember Me</label>
-                    </div>
-                    <p>Need Help?</p>
-                </div>
-            </form>
-        </div>
+        { log ? <SignIn onClick={logIn}/> : <SignUp onClick={logIn}/>}
       </div>
     </div>
   );
 };
 
 export default Login;
+
