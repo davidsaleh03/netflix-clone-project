800f0b80d20e24f260d55261ef6d14196eb681a7
David <davidsaleh03@gmail.com>
2026-01-05 01:11:35 -0800

    major progress

--- Player/Player.jsx
+++ Player/Player.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { Link, useParams } from 'react-router-dom'
 
 const Player = () => {
+const {id} = useParams()
+
   return (
     <div className='player'>
+      <Link to={'/'}>
+      <img src={back_arrow_icon} alt="" />
+      </Link>
+      <iframe width='90%' height='90%' 
+      src={`https://www.youtube.com/embed/${id}`}
+      title='trailer' frameBorder='0' allowFullScreen>
+      </iframe>
 
     </div>
   )
